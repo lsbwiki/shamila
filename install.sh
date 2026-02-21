fb019c5461e75d243ed197cea5c1d489f20a6ed7
lsbwiki <lingdian@live.com>
2026-02-22 02:50:42 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ pkill node || true
 echo "--- 测速任务开始 (跳过 Node 编译) ---"
 # 直接执行你的 python 测速逻辑
 wget -O speedtest.py https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-python3 speedtest.py --single --no-upload
-echo "--- 测速结束 ---"
+python3 speedtest.py --single --no-upload 2>&1 | tee /app/speed.log
+cat /app/speed.log
 tail -f /dev/null
